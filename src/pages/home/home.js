880a15ff12aab7fbac48154be54450207171f2b2
Pablo Martínez <pmartinez@angeldasilva.com>
2020-02-17 12:08:48 -0500

    variables

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import '../../resources/styles/home/home.scss';
@@ -13,20 +12,3 @@ function Home() {
 }
 
 export default Home;
-=======
-import React from 'react';
-
-import '../../resources/styles/home/home.scss';
-import LefHome from './leftHome'
-
-function Home() {
-  return (
-    <div className="row content-home">
-      <LefHome/>
-      <div className="back_1">asdasd</div>
-    </div>
-  );
-}
-
-export default Home;
->>>>>>> 7b37c4444486b9c0497d4347b124b18c97ab2a78
