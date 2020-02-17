7b37c4444486b9c0497d4347b124b18c97ab2a78
pmartinez-angeldasilva <60005213+pmartinez-angeldasilva@users.noreply.github.com>
2020-02-17 12:00:38 -0500

    Landing Expectativa Estructura

---
+++ src/pages/home/leftHome.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import '../../resources/styles/home/homeLeft.scss';
+
+
+
+function LeftHome() {
+    
+  return (
+    <div  className="left-home content-page">
+      <button className="btn btn-danger btn_run">Cargar tu cv</button>
+    </div>
+  );
+}
+
+
+
+
+export default LeftHome;
