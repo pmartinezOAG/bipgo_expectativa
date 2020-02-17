e421b0217dbfbfaf54f134265c0be6c8ef096af2
Pablo Martínez <pmartinez@angeldasilva.com>
2020-02-17 12:18:08 -0500

    Ultimos cambios estructura

--- src/pages/home/leftHome.js
+++ src/pages/home/leftHome.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import '../../resources/styles/home/homeLeft.scss';
@@ -18,24 +17,3 @@ function LeftHome() {
 
 
 export default LeftHome;
-=======
-import React from 'react';
-
-import '../../resources/styles/home/homeLeft.scss';
-
-
-
-function LeftHome() {
-    
-  return (
-    <div  className="left-home content-page">
-      <button className="btn btn-danger btn_run">Cargar tu cv</button>
-    </div>
-  );
-}
-
-
-
-
-export default LeftHome;
->>>>>>> 7b37c4444486b9c0497d4347b124b18c97ab2a78
