37756faee4624f165421cd440d84f71a7bd06a4b
Pablo Martínez <pmartinez@angeldasilva.com>
2020-02-18 09:17:01 -0500

    Home Left Estructure

--- src/pages/gameover/gameover.js
+++ src/pages/gameover/gameover.js
@@ -1,14 +1,19 @@
 import React ,{useState} from 'react';
+import { Link } from 'react-router-dom';
 
-import '../../resources/styles/home/homeLeft.scss';
+// style
+import '../../resources/styles/gameover/gameover.scss';
+
 
 
 function Gameover() {
 
 
   return (
-    <div  className="left-home">
-      game over
+    <div  className="game-over">
+      <h1>GAME<br/>OVER</h1>
+      <p>tienes una segunda oportunidad</p>
+      <Link to="/">COMIENZA DE NUEVO</Link>
     </div>
   );
 };
