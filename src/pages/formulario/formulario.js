e4289f2ed5782cab6c98c1ccb991143476e4ba2e
a-giusti <agiusti@angeldasilva.com>
2020-02-18 21:44:23 -0500

    Creado formulario

--- src/pages/formulario/formulario.js
+++ src/pages/formulario/formulario.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import { useForm } from 'react-hook-form'
 
 import '../../resources/styles/formulario/formulario.scss';
 
 function Formulario() {
+
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => { console.log(data) }
+
   return (
     <div className="row content-home no-gutters">
       <div className="left-div">
-
+        <div className="message-container">
+          Tomaste
+        </div> 
       </div>
 
       <div className="right-div">
         <div className="form-container">
+          <form onSubmit={handleSubmit(onSubmit)} className="form-grid">
 
+            <div className="form-group">
+              <label>Nombres</label>
+              <input name="nombres" defaultValue="test" ref={register} />
+            </div>
+
+            <div className="form-group">
+              <label>Apellido</label>
+              <input name="apellido" ref={register({ required: true })} />
+            </div>
+
+            <div className="form-group">
+              <label>Email</label>
+              <input name="email" ref={register({
+                required: 'Required',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "invalid email address"
+                }
+              })}/>
+              {errors.email && <span>This field is required</span>}
+            </div>
+
+            <input type="submit" className="submit-button" value="Regístrate" />
+          </form>
         </div>
       </div>
     </div>
